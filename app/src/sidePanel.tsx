c64a3190599d8f8834844b3c6bce8a6503d43802
yonghyeun <ttddcc119@naver.com>
2024-11-25 13:14:42 +0900

    feat[#4] : SidePanel 예시 view 생성

---
+++ app/src/sidePanel.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { SidePanelPage } from "./pages";
+import "./styles.css";
+
+ReactDOM.createRoot(document.body).render(
+  <React.StrictMode>
+    <SidePanelPage />
+  </React.StrictMode>
+);
