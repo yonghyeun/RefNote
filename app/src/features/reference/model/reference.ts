5760a4d1bc1147d671cc151a09ce8592a66a7c70
yonghyeun <ttddcc119@naver.com>
2024-11-28 00:36:31 +0900

    feat[#8] : saveReferenceOnTab 메소드 기존 storage 덮어 씌우도록 로직 변경

--- app/src/features/reference/model/reference.ts
+++ app/src/features/reference/model/reference.ts
@@ -8,6 +8,7 @@ const getReferenceData = (): ReferenceData => {
     faviconUrl: (document.querySelector('link[rel="icon"]') as HTMLLinkElement)
       ?.href,
     date: new Date().getTime(),
+    isPicked: false,
   };
 };
 
@@ -24,15 +25,24 @@ export const saveReferenceOnTab = async (
     target: { tabId },
     func: getReferenceData,
   });
-  const referenceData = result.result as ReferenceData;
+  const newReferenceData = result.result as ReferenceData;
+
+  // 기존에 저장된 데이터를 가져옵니다.
+  // key 값을 null 로 하면 모든 데이터를 가져옵니다.
+  const oldValue = await chrome.storage.sync.get(null);
+  const changedReferenceData = {
+    ...oldValue.referenceData,
+    [newReferenceData.date]: newReferenceData,
+  };
 
   // referenceData를 저장합니다.
   // 이 때 여러 브라우저에서 접근 가능하도록 sync storage를 사용합니다.
   // ! sync storage는 최대 100kb , 최대 512개의 항목을 저장 할 수 있다.
   // 인덱스 역할을 할 key 값은 date로 설정합니다.
-  await chrome.storage.sync.set<{ [key in number]: ReferenceData }>({
-    [referenceData.date]: referenceData,
+  await chrome.storage.sync.set<{ referenceData: ReferenceData }>({
+    ...oldValue,
+    referenceData: changedReferenceData,
   });
 
-  return referenceData;
+  return changedReferenceData;
 };
