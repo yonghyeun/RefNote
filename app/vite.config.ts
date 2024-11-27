b7fc7c4aaf7d0078612494de1295247495bb8092
yonghyeun <ttddcc119@naver.com>
2024-11-27 19:13:39 +0900

    refactor[#6] : 더 이상 사용되지 않는 generateManifest 함수 제거

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,25 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import webExtension, { readJsonFile } from "vite-plugin-web-extension";
+import webExtension from "vite-plugin-web-extension";
 import path from "path";
-
-function generateManifest() {
-  const manifest = readJsonFile("src/manifest.json");
-  const pkg = readJsonFile("package.json");
-  return {
-    name: pkg.name,
-    description: pkg.description,
-    version: pkg.version,
-    ...manifest,
-  };
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     webExtension({
-      manifest: generateManifest,
+      manifest: "src/manifest.json",
     }),
   ],
   resolve: {
