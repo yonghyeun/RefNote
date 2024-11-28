d4a4f19001a15b83334ac759aee612aec2a7e46f
yonghyeun <ttddcc119@naver.com>
2024-11-28 12:51:44 +0900

    feat[#8] : chromeStorage와 싱크로나이즈 된 store 생성

--- app/src/shared/store/chromeStorage.tsx
+++ app/src/shared/store/chromeStorage.tsx
@@ -1,65 +1,68 @@
 import { useState, useEffect, createContext, useContext } from "react";
 
-/**
- * Chrome Storage의 데이터들에 대한 타입입니다.
- * - UnPickedReferenceData: 사용자가 선택하지 않은 ReferenceData
- * - PickedReferenceDate: 사용자가 선택한 ReferenceData , id 값이 존재 한다.
- */
-interface UnPickedReferenceData extends ReferenceData {
-  isPicked: false;
-}
-
-interface PickedReferenceDate extends ReferenceData {
-  isPicked: true;
-  id: number;
-}
-
 type ReferenceDataInStorage = UnPickedReferenceData | PickedReferenceDate;
 
-interface ChromeStorage extends Record<string, ReferenceDataInStorage> {}
-type ChangeChromeStorage = (
-  changeCallback: (chromeStorage: ChromeStorage) => ChromeStorage
-) => void;
+interface ChromeStorage {
+  reference: Record<ReferenceDataInStorage["title"], ReferenceDataInStorage>;
+}
+
+const chromeStorageInitialValue: ChromeStorage = {
+  reference: {},
+};
 
 const ChromeStorageContext = createContext<{
   chromeStorage: ChromeStorage | null;
-  changeChromeStorage: ChangeChromeStorage;
 } | null>(null);
 
+// ChromeStorageProvider 는 오로지 readonly 형태인 chrome.storage.sync 데이터를 메모리에 저장한 chromeStorage 객체만을 반환합니다.
+// 상태 변경 흐름의 통일성을 위해 모든 상태 변경은 chromeStorage 객체를 통해 이루어집니다.
 export const ChromeStorageProvider = ({
   children,
 }: {
   children: React.ReactNode;
 }) => {
-  const [chromeStorage, setChromeStorage] = useState<ChromeStorage | null>({});
-
-  const changeChromeStorage: ChangeChromeStorage = async (changeCallback) => {
-    if (!chromeStorage) {
-      return;
-    }
-    const changedChromeStorage = changeCallback(chromeStorage);
-
-    try {
-      setChromeStorage(changedChromeStorage);
-      await chrome.storage.sync.set(changedChromeStorage);
-    } catch (error) {
-      // TODO : notification api 이용하여 변경하기
-      console.error("chrome.storage.sync.set error", error);
-      setChromeStorage(chromeStorage);
-    }
-  };
+  const [chromeStorage, setChromeStorage] = useState<ChromeStorage>(
+    chromeStorageInitialValue
+  );
 
   useEffect(() => {
-    // chrome.storage.sync 에서 데이터를 가져와 chromeStorage에 저장합니다.
-    chrome.storage.sync.get(null, (data) => {
-      setChromeStorage(data);
+    // chrome.storage.sync.get 을 통해 chrome.storage.sync 의 데이터를 가져옵니다.
+    chrome.storage.sync.get(null, (chromeStorage) => {
+      setChromeStorage(chromeStorage as ChromeStorage);
     });
+
+    // chrome.storage.onChanged.addListener 를 통해 chrome.storage.sync 의 데이터를 실시간으로 감지합니다.
+    const synchronizeChromeStorage = (change: chrome.storage.StorageChange) => {
+      // 구글 크롬 스토리지의 onChange 이벤트의 changeEvent 는 다음과 같이 생겼습니다.
+      // {[key in string] : {oldValue: any, newValue: any}}
+      // newValue 만을 이용해 해당 스토리지의 데이터를 변경합니다.
+      const updatedChromeStorage = Object.entries(change).reduce(
+        (chromeStorage, [key, value]) => {
+          return {
+            ...chromeStorage,
+            [key]: value.newValue,
+          };
+        },
+        {} as ChromeStorage
+      );
+
+      setChromeStorage(updatedChromeStorage);
+
+      if (process.env.NODE_ENV === "development") {
+        console.group("chrome.storage.onChange");
+        console.dir(updatedChromeStorage);
+        console.groupEnd();
+      }
+    };
+
+    chrome.storage.onChanged.addListener(synchronizeChromeStorage);
+    return () => {
+      chrome.storage.onChanged.removeListener(synchronizeChromeStorage);
+    };
   }, []);
 
   return (
-    <ChromeStorageContext.Provider
-      value={{ chromeStorage, changeChromeStorage }}
-    >
+    <ChromeStorageContext.Provider value={{ chromeStorage }}>
       {children}
     </ChromeStorageContext.Provider>
   );
