6d5a5c2287403e38a5e7a71d820f3b9672acd6e8
yonghyeun <ttddcc119@naver.com>
2024-11-29 00:53:27 +0900

    fix[#16] : 노트 초기화 버튼 아이콘 말고 문구로 변경

--- app/src/features/reference/ui/ResetReferenceButton.tsx
+++ app/src/features/reference/ui/ResetReferenceButton.tsx
@@ -1,5 +1,5 @@
 import { chromeStorageInitialValue } from "@/shared/store/chromeStorage";
-import { IconButton } from "@/shared/ui/button";
+import { Button } from "@/shared/ui/button";
 import styles from "./styles.module.css";
 
 export const ResetReferenceButton = () => {
@@ -10,27 +10,12 @@ export const ResetReferenceButton = () => {
   // 색상으로 약간 주의를 줄 수 있는 빨간색 아이콘 버튼을 만들어주세요.
 
   return (
-    <IconButton
+    <Button
       aria-label="Reset reference"
       className={styles.resetButton}
       onClick={handleResetReference}
     >
-      <svg
-        fill="#000000"
-        width="1rem"
-        height="1rem"
-        viewBox="0 0 1920 1920"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M960 0v112.941c467.125 0 847.059 379.934 847.059 847.059 0 467.125-379.934 847.059-847.059 847.059-467.125 0-847.059-379.934-847.059-847.059 0-267.106 126.607-515.915 338.824-675.727v393.374h112.94V112.941H0v112.941h342.89C127.058 407.38 0 674.711 0 960c0 529.355 430.645 960 960 960s960-430.645 960-960S1489.355 0 960 0"
-          fillRule="evenodd"
-          stroke="#000000"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
-    </IconButton>
+      Reset Reference Note
+    </Button>
   );
 };
