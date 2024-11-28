8c4badfa82ac7f447380489f5f7f147dc2e2692f
ChoiYongHyeun <123540354+yonghyeun@users.noreply.github.com>
2024-11-28 22:04:54 +0900

    [Feat] 액션 이벤트 핸들러로 사이드 패널 열리도록 기능 추가 (#15)
    * refactor[#14] : popup html 제거
    
    * feat[#14] : 액션 이벤트 핸들러로 사이드 패널 열도록 기능 추가
    
    * refactor[#14] : 이제 사용 되지 않는 openSidePanel 핸들러 삭제
    
    * refactor[#14] : 이제 해당 하지 않는 주석 제거

--- app/src/pages/SidePanel.tsx
+++ app/src/pages/SidePanel.tsx
@@ -1,24 +1,10 @@
 import { ReferenceItem, ReferenceSaveButton } from "@/features/reference/ui";
 import { useChromeStorage } from "@/shared/store/chromeStorage";
-import { Button } from "@/shared/ui";
 import styles from "./pages.module.css";
 
 export const SidePanelPage = () => {
   const { chromeStorage } = useChromeStorage();
   const { reference } = chromeStorage;
-
-  const handleOpenSidePanel = async () => {
-    const [tab] = await chrome.tabs.query({
-      active: true,
-      currentWindow: true,
-    });
-
-    if (!tab || !tab.id) {
-      return;
-    }
-
-    chrome.runtime.sendMessage({ message: "openSidePanel", tabId: tab.id });
-  };
 
   return (
     <>
