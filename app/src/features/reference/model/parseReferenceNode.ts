72b9665065431fc43c8a0bfb78674dc23e589b96
yonghyeun <ttddcc119@naver.com>
2024-12-01 00:51:06 +0900

    refactor[#18] : parseReferenceNode 메소드 주석 추가

--- app/src/features/reference/model/parseReferenceNode.ts
+++ app/src/features/reference/model/parseReferenceNode.ts
@@ -15,6 +15,18 @@ interface WrittenReferenceInfo {
     nextNode: Node;
   } | null;
 }
+/**
+ * 해당 메소드는 인수로 받은 currentNode와 다음노드의 textContent의 값을 이용해 다음과 같은 값들을 검출해냅니다.
+ * - bracketOnlyMatch.singleBracketMatch : 현재 노드의 textContent에서 [숫자] 형태의 데이터를 검출합니다.
+ * - bracketOnlyMatch.doubleBracketMatch : 현재 노드의 textContent에서 [[숫자]] 형태의 데이터를 검출합니다.
+ * - referenceMatchInSameTag : 현재 노드의 textContent에서 [[숫자]](url) 형태의 데이터를 검출합니다.
+ * - referenceMatchInNextTag.splitDoubleBracket : 현재 노드의 textContent에서 [[숫자]] 형태의 마지막 데이터를 검출합니다.
+ * - referenceMatchInNextTag.nextNodeUrlMatch : 다음 노드의 textContent에서 (url) 형태의 데이터를 검출합니다.
+ * - referenceMatchInNextTag.nextNode : 다음 노드를 반환합니다.
+ *
+ * @param currentNode : 탐색 할 현재의 노드
+ * @returns WrittenReferenceInfo | null : 해당 노드의 레퍼런스 정보를 반환합니다. 레퍼런스 정보가 없다면 null을 반환합니다.
+ */
 export const parseReferenceNode = (
   currentNode: Node
 ): WrittenReferenceInfo | null => {
