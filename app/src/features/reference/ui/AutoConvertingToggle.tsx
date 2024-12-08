4820e9a7f96245e0ead058a18cf932362a4b290b
yonghyeun <ttddcc119@naver.com>
2024-12-09 00:47:41 +0900

    feat[#29] : AutoConverting 이 실행되는 순서 선언적에서 명령적으로 변경

--- app/src/features/reference/ui/AutoConvertingToggle.tsx
+++ app/src/features/reference/ui/AutoConvertingToggle.tsx
@@ -5,7 +5,6 @@ import { useEffect, useState } from "react";
 export const AutoConvertingToggle = () => {
   const { chromeStorage, setChromeStorage } = useChromeStorage();
   const { autoConverting } = chromeStorage;
-  const [isDisabled, setIsDisabled] = useState<boolean>(false);
 
   useEffect(() => {
     (async function () {
@@ -14,42 +13,55 @@ export const AutoConvertingToggle = () => {
         currentWindow: true,
       });
 
-      // autoConvertingToggle이 마운트 된 이후 autoConverting 스크립트를 삽입합니다.
-
       if (!tab || !tab.id || tab.url !== "https://velog.io/write") {
         return;
       }
 
-      chrome.scripting.executeScript({
+      await chrome.scripting.executeScript({
         target: { tabId: tab.id },
         files: ["src/autoConverting.js"],
+      });
+
+      await chrome.tabs.sendMessage(tab.id, {
+        message: "SetAutoConverting",
+        data: autoConverting,
       });
     })();
   }, []);
 
-  useEffect(() => {
-    (async function () {
+  const handleToggle = async () => {
+    try {
       const [tab] = await chrome.tabs.query({
         active: true,
         currentWindow: true,
       });
 
       if (!tab || !tab.id) {
-        return;
+        throw new Error(
+          "현재 탭 정보를 가져오는데 실패했습니다. 다시 시도해주세요"
+        );
       }
-      try {
-        await chrome.tabs.sendMessage(tab.id, {
-          message: "SetAutoConverting",
-          data: autoConverting ? "on" : "off",
-        });
-      } catch (e) {
-        // 에러가 나는 경우에는 스크립트가 삽입되지 않은 경우, 즉 올바른 경로가 아닌 곳에서 autoConvertingToggle을 누른 경우입니다.
-        // 이 때는 해당 버튼을 disabled 상태로 만들어 오류가 발생하지 않도록 합니다.
 
-        setIsDisabled(true);
+      if (tab.url !== "https://velog.io/write") {
+        throw new Error(
+          "해당 기능은 벨로그 > 글쓰기에서만 사용 가능한 기능입니다."
+        );
       }
-    })();
-  }, [autoConverting]);
+
+      await chrome.tabs.sendMessage(tab.id, {
+        message: "SetAutoConverting",
+        data: autoConverting ? "on" : "off",
+      });
+
+      setChromeStorage((prev) => ({
+        ...prev,
+        autoConverting: !prev.autoConverting,
+      }));
+    } catch (error) {
+      // TODO 알림 처리 하기
+      console.error(error);
+    }
+  };
 
   return (
     <label className={styles.toggleLabel}>
@@ -63,15 +75,7 @@ export const AutoConvertingToggle = () => {
           type="checkbox"
           className={styles.checkBox}
           checked={autoConverting}
-          onChange={() => {
-            setChromeStorage((prev) => {
-              return {
-                ...prev,
-                autoConverting: !prev.autoConverting,
-              };
-            });
-          }}
-          disabled={isDisabled}
+          onChange={handleToggle}
         />
         <span className={styles.toggleSlider} />
       </div>
