3db4b72fa415d393fea30314eec49858f76e65e5
ChoiYongHyeun <123540354+yonghyeun@users.noreply.github.com>
2024-12-12 18:03:38 +0900

    [Hotfix] window 조작하는 콘텐트 스크립트 자동으로 삽입 되도록 기능 수정 (#46)
    * feat[#45] : 대규모 업데이트 전 기억하기 위한 커밋
    
    * feat[#45] : content script 자동 삽입으로 변경
    
    * fix[#45] : 닫은 채로도 자동 완성 기능 쓸 수 있도록 기능 추가
    
    - 어차피 콘텐트 스크립트는 자동으로 삽입되잖아?!
    
    * feat[#45] : 알림 기능 background 로 이동
    
    * feat[#45] : executeScript 하지 않고 콘텐트 스크립트 삽입은 manifest.json 을 통해서만 일어나도록 기능 변경

--- app/src/features/reference/ui/AutoConvertingToggle.tsx
+++ app/src/features/reference/ui/AutoConvertingToggle.tsx
@@ -1,112 +1,27 @@
 import { useChromeStorage } from "@/shared/store";
 import styles from "./styles.module.css";
 import { useEffect } from "react";
+import { getVelogTab } from "../model";
 
 export const AutoConvertingToggle = () => {
   const { chromeStorage, setChromeStorage } = useChromeStorage();
   const { autoConverting } = chromeStorage;
 
-  useEffect(() => {
-    (async function () {
-      const [tab] = await chrome.tabs.query({
-        active: true,
-        currentWindow: true,
-      });
-
-      if (!tab || !tab.id || tab.url !== "https://velog.io/write") {
-        const chromeStorage = (await chrome.storage.sync.get(
-          null
-        )) as ChromeStorage;
-
-        setChromeStorage(() => ({
-          ...chromeStorage,
-          autoConverting: false,
-        }));
-
-        return;
-      }
-
-      await chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        files: ["src/autoConverting.js"],
-      });
-
-      await chrome.tabs.sendMessage(tab.id, {
-        message: "SetAutoConverting",
-        data: autoConverting,
-      });
-    })();
-  }, []);
-
-  const handleToggle = async () => {
+  const initialize = async () => {
     try {
-      const [tab] = await chrome.tabs.query({
-        active: true,
-        currentWindow: true,
-      });
-
-      if (!tab || !tab.id || !tab.url) {
-        throw new Error(
-          "현재 탭 정보를 가져오는데 실패했습니다. 다시 시도해주세요"
-        );
-      }
-
-      if (!tab.url.includes("https://velog.io/write")) {
-        throw new Error(
-          "해당 기능은 벨로그 > 글쓰기에서만 사용 가능한 기능입니다."
-        );
-      }
+      const tab = await getVelogTab();
+      const { autoConverting } =
+        await chrome.storage.sync.get("autoConverting");
 
       await chrome.tabs.sendMessage(tab.id, {
         message: "SetAutoConverting",
-        data: autoConverting ? "off" : "on",
+        data: autoConverting ? "on" : "off",
       });
-
-      setChromeStorage((prev) => ({
-        ...prev,
-        autoConverting: !prev.autoConverting,
-      }));
     } catch (error) {
-      // 이러한 경우에 대한 에러 핸들링을 해야 합니다.
-      // 확장 프로그램이 지정된 경로가 아닌 곳에서 열린 후
-      // 열린 채로 지정된 경로로 이동한 경우 사용 가능하지만 스크립트가 삽입 되지 않았기 때문에 에러가 발생 할 수 있습니다.
-
-      const [tab] = await chrome.tabs.query({
-        active: true,
-        currentWindow: true,
+      await chrome.runtime.sendMessage({
+        message: "NotifyError",
+        data: (error as Error).message || "알 수 없는 오류가 발생하였습니다.",
       });
-
-      if (
-        tab &&
-        tab.id &&
-        tab.url &&
-        tab.url.includes("https://velog.io/write")
-      ) {
-        await chrome.scripting.executeScript({
-          target: { tabId: tab.id },
-          files: ["src/autoConverting.js"],
-        });
-        handleToggle();
-        return;
-      }
-
-      const errorMessage = (error as Error).message;
-
-      await chrome.notifications.create(
-        errorMessage,
-        {
-          type: "basic",
-          iconUrl: "/icon/128.png",
-          title: "오류 발생",
-          message: errorMessage,
-        },
-        () => {
-          // 1초 후에 해당 알림 닫히게 하기
-          setTimeout(() => {
-            chrome.notifications.clear(errorMessage);
-          }, 1000);
-        }
-      );
 
       const chromeStorage = (await chrome.storage.sync.get(
         null
@@ -118,6 +33,31 @@ export const AutoConvertingToggle = () => {
       }));
     }
   };
+
+  const handleToggle = async () => {
+    try {
+      const tab = await getVelogTab();
+
+      await chrome.tabs.sendMessage(tab.id, {
+        message: "SetAutoConverting",
+        data: autoConverting ? "off" : "on",
+      });
+
+      setChromeStorage((prev) => ({
+        ...prev,
+        autoConverting: !autoConverting,
+      }));
+    } catch (error) {
+      chrome.runtime.sendMessage({
+        message: "NotifyError",
+        data: (error as Error).message || "알 수 없는 오류가 발생하였습니다.",
+      });
+    }
+  };
+
+  useEffect(() => {
+    initialize();
+  }, []);
 
   return (
     <label className={styles.toggleLabel}>
