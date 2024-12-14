67c7894d07374f8907484864b24a64bbcaa157a8
yonghyeun <ttddcc119@naver.com>
2024-12-15 01:47:57 +0900

    feat[#49] : autoConverting 초기화 로직 수정 , handleToggle 로직 수정
    - 에러 핸들링 로직이 추가된 toggling 메소드를 통하여 content script 가 삽입되지 않은 경우에 동적으로 삽입하도록 기능 추가

--- app/src/features/reference/ui/AutoConvertingToggle.tsx
+++ app/src/features/reference/ui/AutoConvertingToggle.tsx
@@ -1,77 +1,91 @@
-import { useChromeStorage } from "@/shared/store";
+import { useChromeStorage, useTab } from "@/shared/store";
 import styles from "./styles.module.css";
 import { useEffect } from "react";
-import { getVelogTab } from "../model";
 import { sendMessage } from "@/shared/lib";
 
 export const AutoConvertingToggle = () => {
   const { chromeStorage, setChromeStorage } = useChromeStorage();
   const { autoConverting } = chromeStorage;
+  const tab = useTab();
 
-  const initialize = async () => {
+  /**
+   * toggling 메소드는 url이 velog.io/write인지 확인된 경우에만 호출 되어야 합니다.
+   * 해당 메소드의 존재 이유는 SPA 로 개발 된 velog 에서 content script가 적절히 삽입 되지 않은 경우
+   * 에러 핸들러를 통해 스크립트를 적절히 삽입하고 다시 toggling 메소드를 호출하기 위함입니다.
+   */
+  const toggling = async (data: "on" | "off", tabId: number) => {
     try {
-      const tab = await getVelogTab();
-
-      if (!tab.url.includes("https://velog.io/write")) {
-        return;
-      }
-
-      const { autoConverting } =
-        await chrome.storage.sync.get("autoConverting");
-
-      await chrome.tabs.sendMessage(tab.id, {
+      await chrome.tabs.sendMessage(tabId, {
         message: "SetAutoConverting",
-        data: autoConverting ? "on" : "off",
-      });
-    } catch (error) {
-      await chrome.runtime.sendMessage({
-        message: "NotifyError",
-        data: (error as Error).message || "알 수 없는 오류가 발생하였습니다.",
+        data,
       });
 
-      const chromeStorage = (await chrome.storage.sync.get(
-        null
-      )) as ChromeStorage;
-
+      // TOOD state 를 변경하는 부분을 분리하여 중복 제거
+      const prev = (await chrome.storage.sync.get(null)) as ChromeStorage;
       setChromeStorage(() => ({
-        ...chromeStorage,
-        autoConverting: false,
+        ...prev,
+        autoConverting: data === "on" ? true : false,
       }));
+    } catch (error) {
+      await chrome.scripting.executeScript({
+        target: { tabId },
+        files: ["src/autoConverting.js"],
+      });
+      toggling(data, tabId);
     }
   };
 
-  const handleToggle = async () => {
-    try {
-      const tab = await getVelogTab();
+  const handleToggle = () => {
+    if (!tab) {
+      return;
+    }
 
-      if (!tab.url.includes("https://velog.io/write")) {
-        await sendMessage({
-          message: "NotifyError",
-          data: "자동 변환 기능은 벨로그 > 글쓰기에서만 가능합니다.",
-        });
-        return;
+    try {
+      const { id, url } = tab;
+
+      if (!id || !url) {
+        throw new Error("현재 탭의 정보를 가져올 수 없습니다.");
       }
 
-      await chrome.tabs.sendMessage(tab.id, {
-        message: "SetAutoConverting",
-        data: autoConverting ? "off" : "on",
-      });
+      if (!url.includes("https://velog.io/write")) {
+        throw new Error(
+          "자동 전환 기능은 벨로그 > 글 작성 페이지에서만 사용할 수 있습니다."
+        );
+      }
 
-      setChromeStorage((prev) => ({
-        ...prev,
-        autoConverting: !autoConverting,
-      }));
+      toggling(autoConverting ? "off" : "on", id);
     } catch (error) {
-      chrome.runtime.sendMessage({
+      sendMessage({
         message: "NotifyError",
-        data: (error as Error).message || "알 수 없는 오류가 발생하였습니다.",
+        data: (error as Error).message,
       });
     }
   };
 
   useEffect(() => {
-    initialize();
-  }, []);
+    if (!tab) {
+      return;
+    }
+
+    try {
+      const { id, url } = tab;
+
+      if (!id || !url) {
+        throw new Error("현재 탭의 정보를 가져올 수 없습니다.");
+      }
+
+      if (!url.includes("https://velog.io/write")) {
+        return;
+      }
+
+      toggling(autoConverting ? "on" : "off", id);
+    } catch (error) {
+      sendMessage({
+        message: "NotifyError",
+        data: (error as Error).message,
+      });
+    }
+  }, [tab]);
 
   return (
     <label className={styles.toggleLabel}>
