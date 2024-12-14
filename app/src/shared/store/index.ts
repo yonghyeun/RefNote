fbcab7255422b5b68d5c6478d6feed747eaf2ec9
yonghyeun <ttddcc119@naver.com>
2024-12-15 01:44:51 +0900

    feat[#49] : tab 을 상태로 저장하도록 기능 수정

--- app/src/shared/store/index.ts
+++ app/src/shared/store/index.ts
@@ -1,1 +1,2 @@
 export * from "./chromeStorage";
+export * from "./tab";
--- app/src/sidePanel.tsx
+++ app/src/sidePanel.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { SidePanelPage } from "./pages";
 import { ChromeStorageProvider } from "./shared/store/chromeStorage";
 import "./styles.css";
+import { TabProvider } from "./shared/store";
 
 ReactDOM.createRoot(document.body).render(
   <React.StrictMode>
     <ChromeStorageProvider>
-      <SidePanelPage />
+      <TabProvider>
+        <SidePanelPage />
+      </TabProvider>
     </ChromeStorageProvider>
   </React.StrictMode>
 );
