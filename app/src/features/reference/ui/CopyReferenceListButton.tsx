95c45e01d4c264847f05151da56b7a9809f3241c
yonghyeun <ttddcc119@naver.com>
2024-12-18 00:48:03 +0900

    [Hotfix] : 복사 완료 후 뜨는 notification silent true 설정 추가

--- app/src/features/reference/ui/CopyReferenceListButton.tsx
+++ app/src/features/reference/ui/CopyReferenceListButton.tsx
@@ -13,8 +13,6 @@ export const CopyReferenceListButton = () => {
       ),
     ].sort((a, b) => a.id - b.id);
 
-    // 마크다운 모드라면 마크다운 문법에 맞게 anchor tag를 만들어줍니다.
-    // 마크다운 모드가 아니라면 일반 텍스트로 만들어줍니다.
     const result = attachedReferences.map(
       ({ title, url, id }) => `[${id}. ${title}](${url})`
     );
@@ -28,6 +26,7 @@ export const CopyReferenceListButton = () => {
         iconUrl: "/icon/128.png",
         title: "복사 완료",
         message: `${reference.filter(({ isWritten }) => isWritten).length} 개의 레퍼런스 목록이 복사되었습니다.`,
+        silent: true,
       },
       () => {
         setTimeout(() => {
