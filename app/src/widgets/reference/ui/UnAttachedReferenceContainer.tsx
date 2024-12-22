16f16a7b64867817416c9ec899942646e8403274
yonghyeun <ttddcc119@naver.com>
2024-12-22 15:59:55 +0900

    feat[#61] : folded 스타일 및 버튼 생성

--- app/src/widgets/reference/ui/UnAttachedReferenceContainer.tsx
+++ app/src/widgets/reference/ui/UnAttachedReferenceContainer.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import styles from "./reference.module.css";
 import { UnAttachedReferenceList } from "@/features/reference/ui";
+import { Button } from "@/shared/ui/button";
 
 interface UnAttachedReferenceContainerProps {
   unAttachedReferenceList: UnAttachedReferenceData[];
@@ -8,15 +10,32 @@ interface UnAttachedReferenceContainerProps {
 export const UnAttachedReferenceContainer = ({
   unAttachedReferenceList,
 }: UnAttachedReferenceContainerProps) => {
+  const [unAttachedReferenceListVisible, setUnAttachedReferenceListVisible] =
+    useState<boolean>(false);
+
   return (
-    <section className={styles.referenceContainer}>
-      <h2>
-        글에 첨부되지 않은 레퍼런스
-        <span>({unAttachedReferenceList.length})</span>
-      </h2>
-      <UnAttachedReferenceList
-        unAttachedReferenceList={unAttachedReferenceList}
-      />
+    <section
+      className={`${styles.referenceContainer} ${unAttachedReferenceListVisible ? "" : styles.folded}`}
+    >
+      <div>
+        <h2>
+          글에 첨부되지 않은 레퍼런스
+          <span>({unAttachedReferenceList.length})</span>
+        </h2>
+        <Button
+          onClick={() => {
+            setUnAttachedReferenceListVisible((prev) => !prev);
+          }}
+          className={styles.foldButton}
+        >
+          {unAttachedReferenceListVisible ? "접기" : "펼치기"}
+        </Button>
+      </div>
+      {unAttachedReferenceListVisible && (
+        <UnAttachedReferenceList
+          unAttachedReferenceList={unAttachedReferenceList}
+        />
+      )}
     </section>
   );
 };
