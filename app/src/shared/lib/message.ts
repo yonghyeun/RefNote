1a1916357d1f6f3cf7110555d2b3c6bd88fc5294
yonghyeun <ttddcc119@naver.com>
2024-12-26 11:27:22 +0900

    feat[#65] : sendMessageToBackground 메소드 생성

--- app/src/shared/lib/message.ts
+++ app/src/shared/lib/message.ts
@@ -16,3 +16,17 @@ export const sendMessageToTab = async <T = undefined>(
     );
   });
 };
+
+export const sendMessageToBackground = async <T = undefined>(
+  message: RequestMessage<number>
+) => {
+  return new Promise<T>((resolve, reject) => {
+    chrome.runtime.sendMessage(message, (response) => {
+      if (chrome.runtime.lastError || !response) {
+        reject(chrome.runtime.lastError?.message || "응답이 없습니다.");
+        return;
+      }
+      resolve(response.data);
+    });
+  });
+};
