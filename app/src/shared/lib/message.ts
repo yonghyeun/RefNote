88802c205ed21decfe5643e7e9aedc0a7380ad90
yonghyeun <ttddcc119@naver.com>
2024-12-29 22:18:07 +0900

    feat[#73] : sendMessage .. 에 loggingError 메소드 추가

--- app/src/shared/lib/message.ts
+++ app/src/shared/lib/message.ts
@@ -1,3 +1,15 @@
+const loggingError = <K = undefined>(
+  errorOriginMessage: RequestMessage<K>,
+  errorMessage: string
+) => {
+  console.group("error occur in message handler");
+  console.group("error origin message");
+  console.table(errorOriginMessage);
+  console.groupEnd();
+  console.trace(errorMessage);
+  console.groupEnd();
+};
+
 export const sendMessageToTab = async <T = void, K = undefined>(
   message: RequestMessage<K>
 ) => {
@@ -7,7 +19,12 @@ export const sendMessageToTab = async <T = void, K = undefined>(
       message,
       (response) => {
         if (chrome.runtime.lastError || !response) {
-          reject(chrome.runtime.lastError?.message || "응답이 없습니다.");
+          const errorMessage =
+            chrome.runtime.lastError?.message || "응답이 없습니다.";
+
+          loggingError(message, errorMessage);
+
+          reject(errorMessage);
           return;
         }
         resolve(response.data);
@@ -22,7 +39,12 @@ export const sendMessageToBackground = async <T = void, K = undefined>(
   return new Promise<T>((resolve, reject) => {
     chrome.runtime.sendMessage(message, (response) => {
       if (chrome.runtime.lastError || !response) {
-        reject(chrome.runtime.lastError?.message || "응답이 없습니다.");
+        const errorMessage =
+          chrome.runtime.lastError?.message || "응답이 없습니다.";
+
+        loggingError(message, errorMessage);
+
+        reject(errorMessage);
         return;
       }
       resolve(response.data);
