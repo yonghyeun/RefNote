5df88dc33c6fd011ad6ea704ce20d65784ace9c9
yonghyeun <ttddcc119@naver.com>
2024-12-29 22:10:34 +0900

    refactor[#73] : sendMessageToTab 타입 수정

--- app/src/shared/lib/message.ts
+++ app/src/shared/lib/message.ts
@@ -1,10 +1,9 @@
-export const sendMessageToTab = async <T = undefined>(
-  tabId: number,
-  message: { message: string }
+export const sendMessageToTab = async <T = void, K = undefined>(
+  message: RequestMessage<K>
 ) => {
   return new Promise<T>((resolve, reject) => {
-    chrome.tabs.sendMessage<{ message: string }, ResponseMessage<T>>(
-      tabId,
+    chrome.tabs.sendMessage<RequestMessage<K>, ResponseMessage<T>>(
+      message.tab.id,
       message,
       (response) => {
         if (chrome.runtime.lastError || !response) {
