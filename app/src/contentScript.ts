d8ae64f5794dc9c8c05c9bbe50d208b215c3cbb7
yonghyeun <ttddcc119@naver.com>
2024-12-31 13:49:05 +0900

    feat[#75] : autoToggling 단순히 상태만 변경하도록 수정

--- app/src/contentScript.ts
+++ app/src/contentScript.ts
@@ -46,58 +46,42 @@ window.addEventListener("unload", () => {
   }
 });
 
-function sendConvertReferenceMessage(event: KeyboardEvent) {
-  // 눌린키가 유효한 키인지 확인 , 방향키나 meta 키 등 포함하지 아니함
-  if (
-    (event.key !== "Backspace" && event.key.length !== 1) ||
-    event.key === " " ||
-    event.key === "Meta" ||
-    event.key === "Alt" ||
-    event.key === "Control" ||
-    event.key === "Shift" ||
-    event.key === "ArrowUp" ||
-    event.key === "ArrowDown" ||
-    event.key === "ArrowLeft" ||
-    event.key === "ArrowRight"
-  ) {
-    return;
-  }
-  if (timer) {
-    clearTimeout(timer);
-  }
-  timer = setTimeout(async () => {
-    const [tab] = await chrome.tabs.query({
-      active: true,
-      currentWindow: true,
-    });
+// function sendConvertReferenceMessage(event: KeyboardEvent) {
+//   // 눌린키가 유효한 키인지 확인 , 방향키나 meta 키 등 포함하지 아니함
+//   if (
+//     (event.key !== "Backspace" && event.key.length !== 1) ||
+//     event.key === " " ||
+//     event.key === "Meta" ||
+//     event.key === "Alt" ||
+//     event.key === "Control" ||
+//     event.key === "Shift" ||
+//     event.key === "ArrowUp" ||
+//     event.key === "ArrowDown" ||
+//     event.key === "ArrowLeft" ||
+//     event.key === "ArrowRight"
+//   ) {
+//     return;
+//   }
+//   if (timer) {
+//     clearTimeout(timer);
+//   }
+//   timer = setTimeout(async () => {
+//     const [tab] = await chrome.tabs.query({
+//       active: true,
+//       currentWindow: true,
+//     });
 
-    if (!tab.id) {
-      return;
-    }
+//     if (!tab.id) {
+//       return;
+//     }
 
-    const { status, data } = await chrome.runtime.sendMessage({
-      message: "ConvertToReference",
-      tab,
-    });
-    chrome.runtime.sendMessage({
-      message: status === "ok" ? "NotifyConvertProcessSuccess" : "NotifyError",
-      data,
-    });
-  }, 500);
-}
-chrome.runtime.onMessage.addListener(
-  ({ message, data }, _sender, sendResponse) => {
-    console.group("RefNote: contentScript.ts 가 메시지를 수신했습니다.");
-    console.table({ message, data });
-    console.groupEnd();
-
-    if (message === "SetAutoConverting") {
-      if (data === "on") {
-        window.addEventListener("keyup", sendConvertReferenceMessage);
-      } else {
-        window.removeEventListener("keyup", sendConvertReferenceMessage);
-      }
-      sendResponse({ status: "ok" });
-    }
-  }
-);
+//     const { status, data } = await chrome.runtime.sendMessage({
+//       message: "ConvertToReference",
+//       tab,
+//     });
+//     chrome.runtime.sendMessage({
+//       message: status === "ok" ? "NotifyConvertProcessSuccess" : "NotifyError",
+//       data,
+//     });
+//   }, 500);
+// }
--- app/src/features/reference/ui/AutoConvertingToggle.tsx
+++ app/src/features/reference/ui/AutoConvertingToggle.tsx
@@ -1,80 +1,8 @@
-import { useEffect } from "react";
-import { useChromeStorage, useTab } from "@/shared/store";
-import { sendMessageToTab, sendMessageToBackground } from "@/shared/lib";
+import { useChromeStorage } from "@/shared/store";
 import styles from "./styles.module.css";
 
 export const AutoConvertingToggle = () => {
   const { chromeStorage, setChromeStorage } = useChromeStorage();
-  const tab = useTab();
-
-  const toggling = async (
-    tab: Tab,
-    autoConverting: boolean,
-    retry: boolean
-  ) => {
-    try {
-      await sendMessageToTab<void, "on" | "off">({
-        tab,
-        message: "SetAutoConverting",
-        data: autoConverting ? "on" : "off",
-      });
-
-      if (autoConverting !== chromeStorage.autoConverting) {
-        setChromeStorage((prev) => ({
-          ...prev,
-          autoConverting,
-        }));
-      }
-    } catch (error) {
-      if (retry) {
-        setTimeout(() => {
-          toggling(tab, autoConverting, false);
-        }, 500);
-        return;
-      }
-
-      sendMessageToBackground<void, string>({
-        message: "NotifyError",
-        data: "자동 변환기능에 사용 할 삽입 될 콘텐트 스크립트를 찾지 못했습니다. 새로고침 후 이용해 주세요",
-        tab,
-      });
-
-      setChromeStorage((prev) => ({
-        ...prev,
-        autoConverting: false,
-      }));
-    }
-  };
-
-  const handleToggle = () => {
-    if (!tab) {
-      return;
-    }
-
-    if (!tab.url.includes("https://velog.io/write")) {
-      sendMessageToBackground<void, string>({
-        message: "NotifyError",
-        data: "자동 전환 기능은 Velog 글쓰기 페이지에서만 사용 가능합니다.",
-        tab,
-      });
-      return;
-    }
-
-    toggling(tab, !chromeStorage.autoConverting, true);
-  };
-
-  useEffect(() => {
-    (async () => {
-      if (!tab || !tab.url.includes("https://velog.io/write")) {
-        return;
-      }
-
-      const { autoConverting } =
-        await chrome.storage.sync.get("autoConverting");
-
-      toggling(tab, autoConverting, false);
-    })();
-  }, [tab]);
 
   return (
     <label className={styles.toggleLabel}>
@@ -88,7 +16,12 @@ export const AutoConvertingToggle = () => {
           type="checkbox"
           className={styles.checkBox}
           checked={chromeStorage.autoConverting}
-          onChange={handleToggle}
+          onChange={() => {
+            setChromeStorage((prev) => ({
+              ...prev,
+              autoConverting: !prev.autoConverting,
+            }));
+          }}
         />
         <span className={styles.toggleSlider} />
       </div>
