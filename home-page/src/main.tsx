198dc46a975caee3fc9502f13338a1922c13fc55
yonghyeun <ttddcc119@naver.com>
2025-01-03 00:19:43 +0900

    chore[#79] : geist design system install

--- home-page/src/main.tsx
+++ home-page/src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { GeistProvider, CssBaseline } from "@geist-ui/core";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <GeistProvider>
+      <CssBaseline />
+      <App />
+    </GeistProvider>
   </StrictMode>
 );
