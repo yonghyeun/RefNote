de93a6b2dd65177622f697e43e9c5e4232462d71
yonghyeun <ttddcc119@naver.com>
2025-01-03 15:28:51 +0900

    refactor[#82] : Button module css -> tailwind css 로 마이그레이션

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -3,7 +3,26 @@
 export default {
   content: ["./src/index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#12b886",
+          dark: "#099268",
+          darker: "#0ca678",
+          disabled: "#38d9a9",
+        },
+        text: {
+          DEFAULT: "#fff",
+          hover: "#e6f7f1",
+          disabled: "#d1d5db",
+        },
+        iconButton: {
+          DEFAULT: "#f0f0f0",
+          hover: "#e0e0e0",
+          active: "#d0d0d0",
+        },
+      },
+    },
   },
   plugins: [],
 };
