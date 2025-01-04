b738dcb29d5e95df6f4fbda2400167e384b5de9b
yonghyeun <ttddcc119@naver.com>
2025-01-04 15:59:40 +0900

    feat[#85] : 전체 경로 레이아웃 생성

--- home-page/src/page/layout.tsx
+++ home-page/src/page/layout.tsx
@@ -1,9 +1,23 @@
-import { Page } from "@geist-ui/core";
+import { BorderNavLink, Stack } from "@/shared/ui";
+import { Page } from "@/shared/ui/Page";
+import { Text } from "@geist-ui/core";
 import { Outlet } from "react-router";
 
 export const MainLayout = () => (
   <Page>
-    <Page.Header>header</Page.Header>
+    <Page.Header>
+      <Stack className="justify-between">
+        <Stack className="items-center">
+          <img src="./128.png" className="w-12 h-12" />
+          <Text h1>RefNote</Text>
+        </Stack>
+        <div>darkMode toggle</div>
+      </Stack>
+      <nav className="flex gap-4">
+        <BorderNavLink to="/">Home</BorderNavLink>
+        <BorderNavLink to="/update">Update</BorderNavLink>
+      </nav>
+    </Page.Header>
     <Page.Content>
       <Outlet />
     </Page.Content>
