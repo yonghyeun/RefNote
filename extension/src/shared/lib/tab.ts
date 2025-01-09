e585d5433005823f08b56c6649c2523506bd243f
yonghyeun <ttddcc119@naver.com>
2025-01-09 14:46:27 +0900

    refactor[#106] : isVelogWritePage 메소드로 변경

--- extension/src/shared/lib/tab.ts
+++ extension/src/shared/lib/tab.ts
@@ -1,2 +1,9 @@
 export const isTab = (tab: chrome.tabs.Tab | undefined): tab is Tab =>
   !!tab && !!tab.id && !!tab.url && !!tab.title;
+
+export const isVelogWritePage = (tab: Tab | null) => {
+  if (!tab) {
+    return false;
+  }
+  return tab.url.includes("https://velog.io/write");
+};
