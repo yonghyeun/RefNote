118d2e4c6584d498f57155896b19c1c968c2fd06
yonghyeun <ttddcc119@naver.com>
2025-01-09 13:02:31 +0900

    feat[#103] : saveErrorProvider , useSaveError .. 훅 생성

--- extension/src/index.tsx
+++ extension/src/index.tsx
@@ -1,13 +1,15 @@
 import ReactDOM from "react-dom/client";
 import { SidePanelPage } from "./pages";
 import { ChromeStorageProvider } from "./shared/store/chromeStorage";
-import { TabProvider } from "./shared/store";
+import { SaveErrorProvider, TabProvider } from "./shared/store";
 import "./styles.css";
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
   <ChromeStorageProvider>
     <TabProvider>
-      <SidePanelPage />
+      <SaveErrorProvider>
+        <SidePanelPage />
+      </SaveErrorProvider>
     </TabProvider>
   </ChromeStorageProvider>
 );
